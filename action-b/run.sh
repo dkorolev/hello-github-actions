9c0844d8dfa3acee3bb020f32efcbdbb14a3145e
Dima Korolev <dmitry.korolev@gmail.com>
2020-09-05 06:47:25 -0700

    Try 17.

--- action-b/run.sh
+++ action-b/run.sh
@@ -2,13 +2,5 @@
 
 cd action-b
 
-which g++
-which clang
-which clang++
-
-g++ --version
-clang --version
-clang++ --version
-
 mkdir -p bin
-clang++ -std=c++17 test.cc -o bin/test && bin/test
+g++ -std=c++17 test.cc -o bin/test && bin/test
